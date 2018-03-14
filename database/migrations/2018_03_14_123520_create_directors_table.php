e26621dc9026b7517450f3b638d944cda2b59a44
Daniel Andersen <nirvasher@gmail.com>
2018-03-14 15:18:55 +0100

    Artist, Actor, Director

---
+++ database/migrations/2018_03_14_123520_create_directors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDirectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('directors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('artist_id')->unsigned();
+            $table->integer('movie_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('directors', function (Blueprint $table) {
+          $table->foreign('artist_id')->references('id')->on('artists');
+          $table->foreign('movie_id')->references('id')->on('movies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('directors');
+    }
+}
