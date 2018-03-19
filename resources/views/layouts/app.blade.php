27287ca4069e93e8a902f85145c640c0ee7161be
Emmie Duphorn <emmieduphorn@hotmail.com>
2018-03-19 15:34:31 +0100

    Fixat layout för filmer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
   <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
     <div class="container">
       <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', 'Laravel') }}</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container">
-@foreach ($movies as $movie)
-<a href="{{route('movies.show', ['id' => $movie->id])}}">{{$movie->title}}</a><br>
-@endforeach
-</div>
+  <div class="container">
+    <div class="card-deck">
+      @foreach ($movies as $movie)
+        <div class="card mb-4" style="flex-basis: 200px; flex-grow: 0;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title"> {{ $movie->title }} </h5>
+            <p class="card-text"> {{ $movie->description }}</p>
+          </div><!-- /card-body -->
+          <div class="card-footer">
+            <a href="{{route('movies.show', ['id' => $movie->id])}}" class="btn btn-success btn-block">Läs mer</a><br>
+          </div><!-- /card-footer -->
+        </div><!-- /card -->
+      @endforeach
+    </div><!-- /card-deck -->
+  </div><!-- /container-->
 @endsection
