fcd85bbec0e09c7c2ec0093f511490d2e77e53bf
Daniel Andersen <nirvasher@gmail.com>
2018-03-19 14:24:55 +0100

    Git-konstighet.

--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,9 @@ Route::get('/genres/create', 'GenreController@create')->name('genres.create');
 Route::post('/genres', 'GenreController@store')->name('genres.store');
 Route::get('/genres/{genre}/edit', 'GenreController@edit')->name('genres.edit');
 Route::get('/genres/{genre}', 'GenreController@show')->name('genres.show');
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 Route::post('/genres/{genre}', 'GenreController@update')->name('genres.update');
->>>>>>> cdfc72a270e93f1a3cb7e2147d87505c4fb865f0
 
 Route::get('/actors', 'ActorController@index')->name('actors.index');
 Route::get('/actors/{actor}', 'ActorController@show')->name('actors.show');
 
-<<<<<<< HEAD
 Route::get('/directors/{director}', 'DirectorController@show')->name('directors.show');
-=======
-Route::post('/genres/{genre}', 'GenreController@update')->name('genres.update');
->>>>>>> master
-=======
-Route::get('/directors/{director}', 'DirectorController@show')->name('directors.show');
->>>>>>> cdfc72a270e93f1a3cb7e2147d87505c4fb865f0
