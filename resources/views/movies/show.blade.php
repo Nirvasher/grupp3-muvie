bc7b13b3c22ae13926517d9944c7aaab68945254
Emmie Duphorn <emmieduphorn@hotmail.com>
2018-03-22 12:25:21 +0100

    Lagt in bootstrap

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -2,90 +2,137 @@
 
 @section('content')
 <div class="container">
-<h3>Titel</h3>{{$movie->title}}<br>
-<h3>Beskrivning</h3>{{$movie->description}}<br>
-<h3>Speltid</h3>{{$movie->runtime}}<br>
-<h3>Releasedatum</h3>
-@if ($movie->releasedate < Carbon\Carbon::now())
-{{$movie->releasedate}}
-@else
-Under produktion, släpps {{$movie->releasedate}}
-@endif
-<br>
-<h3>Betyget för filmen</h3>
-<?php
-$countedRatings = $movie->ratings->count();
-$sumRatings = 0;
-foreach ($movie->ratings as $rating) {
-  $sumRatings += $rating->rating;
-}
-if($countedRatings) {
-  $averageRating = $sumRatings/$countedRatings;
-  echo $averageRating.'<br>';
-  echo 'Antal betyg:'.$countedRatings;
-}
-?>
-@auth
-<h3>Betygsätt</h3>
-<form action="{{route('ratings.store', ['id' => $movie->id])}}" method="post">
-  @csrf
-  <select class="form-control" name="rating">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-  </select>
-  <button type="submit" name="button" class="btn btn-primary">Lägg till betyg</button>
-</form>
-@endauth
-<h3>Posterbild</h3>
-@empty($movie->image)
--
-@else
-<img src="{{ asset('storage/'.$movie->image->url) }}"><br>
-@endempty
-<h3>Regissör</h3>
-@empty($movie->director)
--
-@else
-<a href="{{route('directors.show', ['id' => $movie->director->id])}}">{{$movie->director->person->name}}</a>
-@endempty
-<h3>Skådespelare</h3>
-@foreach ($movie->actors as $actor)
-<a href="{{route('actors.show', ['id' => $actor->id])}}">{{$actor->person->name}}</a><br>
-@endforeach
-@if (Auth::check() && Auth::user()->hasRole('editor'))
-<a href="{{route('actors.add', ['id' => $movie->id])}}" class="btn btn-primary">Lägg till skådespelare</a>
-@endif
-<h3>Genrer</h3>
-@foreach ($movie->genres as $genre)
-<a href="{{route('genres.show', ['id' => $genre->id])}}">{{$genre->name}}</a><br>
-@endforeach
-@if (Auth::check() && Auth::user()->hasRole('editor'))
-<form action="{{ route('movies.addGenre', ['id' => $movie->id])}}" method="post">
-  @csrf
-  <select class="form-control" name="genre">
-    @foreach ($genres as $genre)
-    <option value="{{$genre->id}}">{{$genre->name}}</option>
-    @endforeach
-  </select>
-  <button type="submit" name="button" class="btn btn-primary">Lägg till genre</button>
-</form>
-@endif
-<h3>Bilder</h3>
-@foreach ($movie->images as $image)
-<img src="{{ asset('storage/'.$image->url) }}"><br>
-@endforeach
-@if (Auth::check() && Auth::user()->hasRole('editor'))
-<a class="btn btn-primary" href="{{ route('images.create', ['movie' => $movie->id]) }}">Lägg till fler bilder</a>
-@endif
-@auth
-<a class="btn btn-primary" href="{{ route('libraries.create', ['movie' => $movie->id]) }}">Lägg till i biblioteket</a>
-@endauth
 
-@if (Auth::check() && Auth::user()->hasRole('editor'))
-<a class="btn btn-primary" href="{{ route('movies.edit', ['movie' => $movie->id]) }}">Ändra</a>
-@endif
-</div>
+  <div class="col-8">
+    <table class="table">
+
+    <tr>
+      <th><h3>Titel</h3></th>
+      <td> {{$movie->title}} @if (Auth::check() && Auth::user()->hasRole('editor'))
+      <a class="btn btn-sm btn-primary" href="{{ route('movies.edit', ['movie' => $movie->id]) }}">Ändra</a>
+      @endif<br>
+    </tr>
+    <tbody>
+      <tr>
+        <th><h3>Beskrivning</h3></th>
+        <td> {{$movie->description}}<br></td>
+      </tr>
+      <tr>
+        <th><h3>Speltid</h3></th>
+        <td> {{$movie->runtime}}<br></td>
+      </tr>
+      <tr>
+        <th><h3>Releasedatum</h3></th>
+        <td>@if ($movie->releasedate < Carbon\Carbon::now())
+        {{$movie->releasedate}}
+        @else
+        Under produktion, släpps {{$movie->releasedate}}
+        @endif
+        <br></td>
+      </tr>
+      <tr>
+        <th><h3>Genre</h3></th>
+        <td>@foreach ($movie->genres as $genre)
+        <a href="{{route('genres.show', ['id' => $genre->id])}}">{{$genre->name}}</a><br>
+        @endforeach
+        @if (Auth::check() && Auth::user()->hasRole('editor'))
+        <form action="{{ route('movies.addGenre', ['id' => $movie->id])}}" method="post">
+          @csrf
+          <select class="form-control" name="genre">
+            @foreach ($genres as $genre)
+            <option value="{{$genre->id}}">{{$genre->name}}</option>
+            @endforeach
+          </select>
+          <button type="submit" name="button" class="btn btn-sm btn-primary">Lägg till genre</button>
+        </form>
+        @endif</td>
+      </tr>
+
+      <tr>
+        <th><h3>Regissör</h3></th>
+        <td>@empty($movie->director)
+        -
+        @else
+        <a href="{{route('directors.show', ['id' => $movie->director->id])}}">{{$movie->director->person->name}}</a>
+        @endempty</td>
+      </tr>
+      <tr>
+        <th><h3>Skådespelare</h3></th>
+        <td>@foreach ($movie->actors as $actor)
+        <a href="{{route('actors.show', ['id' => $actor->id])}}">{{$actor->person->name}}</a><br>
+        @endforeach
+        @if (Auth::check() && Auth::user()->hasRole('editor'))
+        <a href="{{route('actors.add', ['id' => $movie->id])}}" class="btn btn-sm btn-primary">Lägg till skådespelare</a>
+        @endif</td>
+      </tr>
+
+      <tr>
+        @auth
+        <th><h3>Betygsätt</h3></th>
+        <td><form action="{{route('ratings.store', ['id' => $movie->id])}}" method="post">
+          @csrf
+          <select class="form-control" name="rating">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <button type="submit" name="button" class="btn btn-sm btn-primary">Lägg till betyg</button>
+        </form>
+        @endauth</td>
+      </tr>
+      <tr>
+        <th><h3>Samlat betyg</h3></th>
+        <td><?php
+        $countedRatings = $movie->ratings->count();
+        $sumRatings = 0;
+        foreach ($movie->ratings as $rating) {
+          $sumRatings += $rating->rating;
+        }
+        if($countedRatings) {
+          $averageRating = $sumRatings/$countedRatings;
+          echo $averageRating.'<br>';
+          echo 'Antal betyg:'.$countedRatings;
+        }
+        ?></td>
+      </tr>
+      <tr>
+        <th><h3>Bilder</h3></th>
+        <td>
+        @foreach ($movie->images as $image)
+        <img src="{{ asset('storage/'.$image->url) }}"><br>
+        @endforeach
+        @if (Auth::check() && Auth::user()->hasRole('editor'))
+        <a class="btn btn-sm btn-primary" href="{{ route('images.create', ['movie' => $movie->id]) }}">Lägg till fler bilder</a>
+        @endif
+        </td>
+        </tr>
+      </table>
+      </div>
+
+  <div class="col-4">
+    <table class="table">
+      <tr>
+        <th><h3>Posterbild</h3></th>
+        <td>@empty($movie->image)
+        -
+        @else
+        <img src="{{ asset('storage/'.$movie->image->url) }}"></td>
+      </tr><br>
+
+        <tr>
+          <td>@auth
+          <a class="btn btn-sm btn-primary" href="{{ route('libraries.create', ['movie' => $movie->id]) }}">Lägg till film i biblioteket</a>
+          @endauth</td>
+        </tr>
+
+          <tr>
+          <td>@if (Auth::check() && Auth::user()->hasRole('editor'))
+          <a class="btn btn-sm btn-primary" href="{{ route('movies.edit', ['movie' => $movie->id]) }}">Redigera</a>
+          @endif</td>
+        </tr>
+      </table>
+    </div>
+  </div>
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,15 +2,36 @@
 @section('content')
 
 <div class="container">
+<h1 class="mb-4"> {{ $user->name }} </h1>
 
-<h1>Profil för: {{ $user->name }}</h1>
-<h3>Bibliotek</h3>
-@foreach ($user->libraries as $library)
-<a href="{{route('movies.show', ['id' => $library->movie->id])}}">{{ $library->movie->title }}</a> Format: {{ $library->format }} <a href="{{route('libraries.edit', ['library' => $library->id])}}" class="btn btn-primary">Ändra</a><br>
-@endforeach
-<h3>Ratings</h3>
-@foreach ($user->ratings as $rating)
-<a href="{{route('movies.show', ['id' => $rating->movie->id])}}">{{$rating->movie->title}}</a> Betyg: {{$rating->rating}}<br>
-@endforeach
+<div class="row">
+  <div class="col-6">
+    <table class="table-body">
+    <h3>Mina filmer:</h3>
+      <table class="table table-striped">
+        @foreach ($user->libraries as $library)
+          <tr>
+            <td>
+              <a href="{{route('movies.show', ['id' => $library->movie->id])}}">{{ $library->movie->title }}</a>
+            </td>
+            <td>
+              Format: {{ $library->format }} <a href="{{route('libraries.edit', ['library' => $library->id])}}" class="btn  btn-sm btn-primary float-right">Ändra</a><br>
+            </td>
+          </tr>
+        @endforeach
+    </table>
+    </div>
+    <div class="col-6">
+      <h3>Betygsatta filmer:</h3>
+      <table class="table">
+        <tr>
+          @foreach ($user->ratings as $rating)
+          <a href="{{route('movies.show', ['id' => $rating->movie->id])}}">{{$rating->movie->title}}</a> Betyg: {{$rating->rating}}<br>
+          @endforeach
+        </tr>
+      </table>
+      </table>
+    </div>
+  </div>
 </div>
 @endsection
